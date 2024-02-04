0e32cdde1e096f94f4a1070008240750875d4fca
Sasha Zaitsev <sashazaitsev2005@gmail.com>
2024-02-04 12:26:46 +0200

    Console app

---
+++ SumConsoleApp/SumConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("\n\nFunction to calculate the sum of two numbers :\n");
+Console.Write("--------------------------------------------------\n");
+
+Console.Write("Enter a number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter another number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nThe sum of two numbers is : {0} \n", n1 + n2);
